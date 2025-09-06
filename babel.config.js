3e10ab69b3fba587b352cb2ba7557c55f6dcf19a
js4941662-max <abrahamtrueba@icloud.com>
2025-09-05 19:35:57 -0500

    Fix EAS build configuration and update app settings

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      // expo-router/babel is deprecated in SDK 50+, babel-preset-expo handles it
-    ],
-  };
-};
